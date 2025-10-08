276b13a37979a6f7931de71573bfac243bdfd997
prathamesh22232 <146662828+prathamesh22232@users.noreply.github.com>
2025-10-08 15:20:45 +0530

    changed db.js

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const mongoURI = 'mongodb://localhost:27017/mini';
+const mongoURI = process.env.MONGO_URI;
 
 const connectToMongo = async () => {
     try {
